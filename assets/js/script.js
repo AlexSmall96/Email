0451589ec1e032482c171bb6339edb1c058e94db
AlexSmall96 <alex.small739@gmail.com>
2023-02-25 13:36:43 +0000

    "change color of flags on click"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const flags = document.getElementsByClassName('fa-flag');
+
     for (let flag of flags){
         flag.addEventListener("click", function(){
-            console.log(flag)
+           if (flag.style.color === 'black'){
+            flag.style.color = 'red';
+           } else {
+            flag.style.color = 'black'
+           }
         })
     }
 });
