b75d84e4d1e3cb2da132c7ac399cb70c29993325
AlexSmall96 <alex.small739@gmail.com>
2023-02-25 12:09:47 +0000

    "add event listener to flags"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const mailRows = document.getElementsByClassName("mail-row")
-
-    
-    const emails = {
-        mail1:
-                    {from:"jon",
-                    subject:"meeting",
-                    content:"can you meet at 1?",
-                    time: "12:35pm"}
-        ,
-        mail2:
-            {from:"bob",
-            subject:"meeting tomorow",
-            content:"can you meet at 2?",
-            time: "13:05pm"}
-        
-};
-
-for (let row of mailRows){
-    row.addEventListener("click", function(){
-        mailId = row.id;
-        console.log(emails[mailId])
-    })
-}
-
+    const flags = document.getElementsByClassName('fa-flag');
+    for (let flag of flags){
+        flag.addEventListener("click", function(){
+            console.log(flag)
+        })
+    }
 });
