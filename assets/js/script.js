730484cb1a106f7b7502507c7fb5fb6070a4fe34
AlexSmall96 <alex.small739@gmail.com>
2023-02-28 12:14:40 +0000

    "add flag turn text red functionality to JS file"

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let flag of flags){
         flag.addEventListener("click", function(){
-           if (flag.style.color === 'black'){
-            flag.style.color = 'red';
+            let mailPreview = document.getElementById(`${this.id}-preview`);
+            if (this.style.color === 'black'){
+            this.style.color = 'red';
+            mailPreview.style.color = 'red';
            } else {
-            flag.style.color = 'black'
+            this.style.color = 'black'
+            mailPreview.style.color = 'black';
            }
         })
     }
+
+    const emails = document.getElementsByClassName('mail-preview');
+
+    for (let email of emails){
+        email.addEventListener("click", function(){
+            email.style.fontWeight = "normal";
+            email.style.backgroundColor = "#f5f8fc";
+        });
+    }
+
+
 });
