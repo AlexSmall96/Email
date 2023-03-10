6cbbb543d85e36ea742cc150d537e08bbc90464a
AlexSmall96 <alex.small739@gmail.com>
2023-03-10 18:48:35 +0000

    "add mouse over event listener to mail"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Click on flag icons to turn text red
     const flags = document.getElementsByClassName('fa-flag');
-
     for (let flag of flags){
         flag.addEventListener("click", function(){
             let mailPreview = document.getElementById(`${this.id}-preview`);
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    // Click on emails to make text not bold 
     const emails = document.getElementsByClassName('mail-preview');
-
     for (let email of emails){
         email.addEventListener("click", function(){
             this.style.fontWeight = "normal";
@@ -23,5 +23,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    const mailList = document.getElementById("mail-list");
+    const mailRows = mailList.getElementsByClassName("row");
+      
+    console.log(mailRows)
+    for (let row of mailRows){
+    
+    
+        row.addEventListener("mouseenter", function(){
+            this.style.boxShadow = "0px 2px 2px grey";
+            this.getElementsByClassName("time")[0].innerHTML = `<i class="fa-solid fa-trash-can"></i><i class="fa-solid fa-envelope-open"></i>`
+            this.getElementsByClassName("time")[0].style.padding = "2px";
+        });
+   
+        row.addEventListener("mouseleave", function(){
+            this.style.boxShadow = "none";
+            this.getElementsByClassName("time")[0].innerHTML  = `14:52pm`;
+        });
+    };
 
 });
